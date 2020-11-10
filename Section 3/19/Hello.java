1e4d05b4a867b00ac33bc5a2ecf2bae33d3d5d88
Rahul Golani <rahulgolani24@live.com>
2020-11-10 17:05:19 +0530

    Introduction

---
+++ Section 3/19/Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+  // main method is the entry point of any java code
+  public static void main(String[] args) {
+    // public is the access modifier
+    // static is the keyword which allows the method to be run without the object
+    // void indicates that method will not return anything
+    // main is the name of the function
+    System.out.println("Hello World");
+
+  }
+}
